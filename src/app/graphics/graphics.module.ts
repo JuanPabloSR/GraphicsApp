fc19e69a8ee2685ccd240b749b1fe98ff1610539
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-03-22 17:04:22 -0500

    folder structure completed

---
+++ src/app/graphics/graphics.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraphicsRoutingModule } from './graphics-routing.module';
+import { BarsComponent } from './pages/bars/bars.component';
+import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
+import { DoughnutComponent } from './pages/doughnut/doughnut.component';
+import { DoughnutHttpComponent } from './pages/doughnut-http/doughnut-http.component';
+import { GraphicBarComponent } from './components/graphic-bar/graphic-bar.component';
+
+
+@NgModule({
+  declarations: [
+    BarsComponent,
+    DoubleBarsComponent,
+    DoughnutComponent,
+    DoughnutHttpComponent,
+    GraphicBarComponent
+  ],
+  imports: [
+    CommonModule,
+    GraphicsRoutingModule
+  ]
+})
+export class GraphicsModule { }
